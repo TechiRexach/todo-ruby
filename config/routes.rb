c8596a44f3a9f3569c2bd9becd4eb84cb54a6341
Techi Rexach <techi90@gmail.com>
2021-05-29 13:54:17 +0200

    editing items

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :lists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'lists#index'
+  get 'home/trash_all_completed'
+  get 'home/trash_all_items'
 end
