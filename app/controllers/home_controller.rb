006326e520357e2989fbcc9a3b0e12980b1571a6
Techi Rexach <techi90@gmail.com>
2021-05-30 14:06:19 +0200

    root_path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ActionController::Base
     def trash_all_completed
         @trash_all = List.where(:completed => true).destroy_all
-        redirect_to lists_url, notice: "All marked list items were successfully deleted" 
+        redirect_to root_url, notice: "All marked list items were successfully deleted" 
     end
 
     def trash_all_items
         @trash_all_items = List.all.destroy_all
-        redirect_to lists_url, notice: "All list items were successfully deleted" 
+        redirect_to root_url, notice: "All list items were successfully deleted" 
     end
 end
